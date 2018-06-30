0b2f55f861f6cc418d347bbbb331d6230683dc6a
mabahandulla <40692963+mabahandulla@users.noreply.github.com>
2018-07-01 00:31:11 +0300

    Класс Vector работает кроме ошибки

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 class Vector{
   constructor(x = 0, y = 0){
     this.x = x;
@@ -8,18 +7,22 @@ class Vector{
   }
 
   plus(vector){
-   if(!vector instanceof(Vector)){
+    try{
+   if(!vector instanceof Vector){
      throw 'Можно прибавлять к вектору только вектор типа Vector.';
    }
     let plusX = this.x + vector.x;
     let plusY = this.y + vector.y;
-    let plusObj = {x: plusX, y: plusY};
-    return plusObj;
+    return new Vector(plusX, plusY);
+    //return PLUS_OBJ;
+    } catch (err){
+      console.log(err.message);
+    }
   }
   times(multiplier){
     let multiplyX = this.x * multiplier;
     let multiplyY = this.y * multiplier;
-    return {x: multiplyX, y: multiplyY};
+    return new Vector(multiplyX, multiplyY);
   }
 }
 
@@ -29,4 +32,3 @@ const finish = start.plus(moveTo.times(2));
 
 console.log(`Исходное расположение: ${start.x}:${start.y}`);
 console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
-
