2db4e8aa111975b22dedd71e1972660a82bfe2a9
mabahandulla <40692963+mabahandulla@users.noreply.github.com>
2018-07-07 13:42:32 +0300

    Actor 07.07.2018

--- game.js
+++ game.js
@@ -1,24 +1,24 @@
 'use strict'
 
-class Vector{
-  constructor(x = 0, y = 0){
+class Vector {
+  constructor(x = 0, y = 0) {
     this.x = x;
     this.y = y;
   }
 
-  plus(vector){
-       
-    if(!(vector instanceof Vector)){
-     throw new Error(`Можно прибавлять к вектору только вектор типа Vector.`);
+  plus(vector) {
+
+    if (!(vector instanceof Vector)) {
+      throw new Error(`Можно прибавлять к вектору только вектор типа Vector.`);
     }
-    
+
     let plusX = this.x + vector.x;
     let plusY = this.y + vector.y;
     return new Vector(plusX, plusY);
 
   }
 
-  times(multiplier){
+  times(multiplier) {
     let multiplyX = this.x * multiplier;
     let multiplyY = this.y * multiplier;
     return new Vector(multiplyX, multiplyY);
@@ -37,21 +37,21 @@ console.log('');
 
 
 
-class Actor{
-  constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed  = new Vector(0, 0)){
+class Actor {
+  constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
 
-     if(!(pos instanceof Vector) || !(size instanceof Vector) || !(speed instanceof Vector)){
-         throw new Error(`В конструктор можно передавать только объект типа Vector.`);
-     }
+    if (!(pos instanceof Vector) || !(size instanceof Vector) || !(speed instanceof Vector)) {
+      throw new Error(`В конструктор можно передавать только объект типа Vector.`);
+    }
 
-     this.pos = pos;
-     this.size = size;
-     this.speed = speed;
+    this.pos = pos;
+    this.size = size;
+    this.speed = speed;
 
 
-     Object.defineProperties(this, {
+    Object.defineProperties(this, {
       left: {
-        get: () => this.pos.x 
+        get: () => this.pos.x
       },
       top: {
         get: () => this.pos.y
@@ -65,28 +65,27 @@ class Actor{
       type: {
         get: () => 'actor'
       }
-     });
+    });
 
   }
 
-  act(){
+  act() {
 
   }
 
 
-  isIntersect(actor){
-    if(!actor || !(actor instanceof Actor)){
+  isIntersect(actor) {
+    if (!actor || !(actor instanceof Actor)) {
       throw new Error(`В метод isIntersect можно передавать только объект типа Actor.`);
     }
- 
-
-    if((this.pos.valueOf() === actor.pos.valueOf() && this.size.valueOf() === actor.size.valueOf()) || (this.pos.valueOf() < actor.pos.valueOf()) || (this.pos.valueOf() === actor.pos.valueOf() && actor.size.valueOf() === -1)) {
-      return false;
-     }
 
 
-     return true;
-   
+
+    return (this === actor || (actor.size.x < 0 && actor.size.y < 0) ||
+    (this.pos.valueOf() < actor.pos.valueOf() && this.size.valueOf() < actor.size.valueOf()) ||
+    (this.pos.x === actor.pos.x || this.pos.y === actor.pos.y)
+    ) ? false : true;
+
   }
 }
 
@@ -101,7 +100,7 @@ items.set('Вторая монета', new Actor(new Vector(15, 5)));
 function position(item) {
   return ['left', 'top', 'right', 'bottom']
     .map(side => `${side}: ${item[side]}`)
-    .join(', ');  
+    .join(', ');
 }
 
 function movePlayer(x, y) {
